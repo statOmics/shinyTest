61a0e8ac250fb66d1bf80ae7b4bcd704fdf02e4c
lieven clement <lclement@lievens-MacBook-Pro.local>
2021-09-23 00:39:23 +0200

    test R 4.1.0

--- launchEvalDecoyShinyApp.R
+++
@@ -1,7 +0,0 @@
-#Line below has to be commented out when users are not working with Rstudio
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-
-maxFileSize=100
-options(shiny.maxRequestSize=maxFileSize*1024^2)
-options(shiny.launch.browser = .rs.invokeShinyWindowExternal)
-shiny::runApp("App-evalDecoys/")
--- launchMSqRobShinyApp.R
+++
@@ -1,8 +0,0 @@
-#Line below has to be commented out when users are not working with Rstudio
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-
-maxFileSize=100
-options(shiny.maxRequestSize=maxFileSize*1024^2)
-options(shiny.launch.browser = .rs.invokeShinyWindowExternal)
-source("App-MSqRob/combineFeatures.R")
-shiny::runApp("App-MSqRob/")
